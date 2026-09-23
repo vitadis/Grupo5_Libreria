003e82edccd0006440e3e59986b6786f44cc1e6f
Joelinho <mamanifloresjoel735@gmail.com>
2026-09-23 09:34:12 +0200

    Refactor Libro class constructor and remove exception

--- Libreria_Project_G5/src/model/Libro.java
+++ Libreria_Project_G5/src/model/Libro.java
@@ -24,7 +24,7 @@ public class Libro {
     }
 
     public Libro() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
     @Override
